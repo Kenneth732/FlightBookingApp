e2eb2a98f4118a702d0317fc4bf5d6a98f4e76df
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-31 17:09:01 +0300

    array object for user destination

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,23 @@
 const searchForm = document.querySelector(".search-form");
 const resultsSection = document.getElementById("resultsSection");
 
+// Sample flight data (Replace this with data fetched from API)
+const flightsData = [
+  {
+    origin: "New York",
+    destination: "Los Angeles",
+    departureDate: "2023-08-15",
+    airline: "Delta Airlines",
+    price: 300,
+    seatsAvailable: 150,
+  },
+  {
+    origin: "Chicago",
+    destination: "San Francisco",
+    departureDate: "2023-08-20",
+    airline: "United Airlines",
+    price: 280,
+    seatsAvailable: 120,
+  },
+];
 
